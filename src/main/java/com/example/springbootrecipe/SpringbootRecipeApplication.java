f4d751b92e9167bdb4e514db16a652c7f8327e66
Gaurav Pancholi <gaurav.pancholi2@gmail.com>
2021-05-16 21:29:32 +0530

    Creating repository from local project

---
+++ src/main/java/com/example/springbootrecipe/SpringbootRecipeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootrecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRecipeApplication.class, args);
+	}
+
+}
